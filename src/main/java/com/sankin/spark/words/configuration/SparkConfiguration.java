73a310d769958910663da6a478750b3d6e82193b
Vlad Sankin <vsankin@dnanexus.com>
2017-07-14 15:10:55 -0700

    use s3 for passing input and output. (#5)

--- src/main/java/com/sankin/spark/words/configuration/SparkConfiguration.java
+++ src/main/java/com/sankin/spark/words/configuration/SparkConfiguration.java
@@ -7,27 +7,32 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+
 @Configuration
 public class SparkConfiguration {
 
 	@Value("${spark.app.name:word-count}")
 	private String appName;
 
-	@Value("${spark.master.uri}")
+	@Value("${spark.master.uri:local}")
 	private String masterUri;
 
 	@Bean
 	public SparkConf sparkConf() {
-		SparkConf sparkConf = new SparkConf()
-				.setAppName(appName)
-				.setMaster(masterUri);
-
-		return sparkConf;
+		return new SparkConf().setAppName(appName).setMaster(masterUri);
 	}
 
 	@Bean
 	public JavaSparkContext javaSparkContext() {
-		return new JavaSparkContext(sparkConf());
+		AWSCredentials credentials = new DefaultAWSCredentialsProviderChain().getCredentials();
+		JavaSparkContext jsc = new JavaSparkContext(sparkConf());
+		jsc.hadoopConfiguration().set("fs.s3.awsAccessKeyId", credentials.getAWSAccessKeyId());
+		jsc.hadoopConfiguration().set("fs.s3.awsSecretAccessKey", credentials.getAWSSecretKey());
+		jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", credentials.getAWSAccessKeyId());
+		jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", credentials.getAWSSecretKey());
+		return jsc;
 	}
 
 	@Bean
--- src/main/java/com/sankin/spark/words/service/impl/WordCountServiceImpl.java
+++ src/main/java/com/sankin/spark/words/service/impl/WordCountServiceImpl.java
@@ -1,23 +1,16 @@
 package com.sankin.spark.words.service.impl;
 
-import static org.apache.spark.sql.functions.col;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
 import org.springframework.stereotype.Service;
 
 import com.sankin.spark.words.service.WordCountService;
-import com.sankin.spark.words.service.model.Word;
 import scala.Tuple2;
 
 @Service
@@ -26,43 +19,13 @@ public class WordCountServiceImpl implements WordCountService {
 	@Inject
 	private JavaSparkContext sc;
 
-	@Inject
-	private SparkSession sparkSession;
-
 	@Override
-	public Map<String, Integer> reduceByKey(List<String> words) {
-
-		// prep data
-		JavaRDD<String> data = sc.parallelize(words);
-
-		// avoid creating a domain object/pojo by using a generic Tuple to group key, count
-		JavaPairRDD<String, Integer> pairs = data.mapToPair(s -> new Tuple2(s, 1));
-
-		// execute, collect
-		return pairs
-				.reduceByKey((a, b) -> a + b)
-				.collectAsMap();
+	public void process(String inputFile, String output) {
+		JavaRDD<String> textFile = sc.textFile(inputFile);
+		JavaPairRDD<String, Integer> counts = textFile
+				.flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+				.mapToPair(word -> new Tuple2<>(word, 1))
+				.reduceByKey((a, b) -> a + b);
+		counts.saveAsTextFile(output + RandomStringUtils.randomAlphabetic(5));
 	}
-
-	@Override
-	public Map<String, Long> groupBy(List<String> words) {
-
-		// prep data
-		List<Word> data = words
-				.stream()
-				.map(Word::new)
-				.collect(Collectors.toList());
-
-		// setup data set
-		Dataset<Row> dataFrame = sparkSession.createDataFrame(data, Word.class);
-
-		// count rows and collect count
-		return dataFrame.groupBy(col("word"))
-			.count()
-			.collectAsList()
-			.stream()
-			.collect(Collectors.toMap(row -> row.getString(0), row -> row.getLong(1)));
-
-	}
-
 }
